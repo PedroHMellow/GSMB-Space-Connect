dec358e1a02ed1335e8a7f8b56dc499e4432f06d
PedroHMellow <israelalvesmello2003@gmail.com>
2026-05-28 16:43:21 -0300

    Instalação de React + Expo e NativeWind

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-5xl text-cyan-300"> Teste 123</Text>
+    </View>
+  );
+}
