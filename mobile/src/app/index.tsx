cb152b02231801711e1ab867e3c22cf56c92491e
PedroHMellow <israelalvesmello2003@gmail.com>
2026-05-29 15:58:49 -0300

    Começando as telas das tabs

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  return <Redirect href="/screen/Stack/login" />;
+  return <Redirect href="/screen/Tabs/home" />;
 }
---
+++ mobile/src/app/screen/Tabs/home.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+    return (
+        <View className="flex-1 items-center justify-center bg-background">
+            <Text className="font-inter text-2xl text-black mb-4">
+                Bem-vindo à Home!
+            </Text>
+        </View>
+    )
+}
